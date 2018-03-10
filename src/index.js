028d6e2a34be530f5cf88f5966edf5198da24587
John Gile <jgile@cliquestudios.com>
2018-03-10 15:39:40 -0600

    fix

--- src/index.js
+++ src/index.js
@@ -19,52 +19,50 @@ const VueGetData = {
             }
         });
 
-        Vue.prototype.$vueData = function () {
-            return {
-                process(response) {
-                    const data = this.getData(response);
-                    const message = this.getMessage(response);
+        Vue.prototype.$vueData = {
+            process(response) {
+                const data = this.getData(response);
+                const message = this.getMessage(response);
 
-                    if (message) {
-                        this.alert(message);
-                    }
+                if (message) {
+                    this.alert(message);
+                }
 
-                    return data;
-                },
-                getMessage(response) {
-                    if (_.has(response, 'meta.message')) {
-                        return _.get(response, 'meta.message');
-                    }
+                return data;
+            },
+            getMessage(response) {
+                if (_.has(response, 'meta.message')) {
+                    return _.get(response, 'meta.message');
+                }
 
-                    if (_.has(response, 'data.meta.message')) {
-                        return _.get(response, 'data.meta.message');
-                    }
+                if (_.has(response, 'data.meta.message')) {
+                    return _.get(response, 'data.meta.message');
+                }
 
-                    if (_.isString(response)) {
-                        return response;
-                    }
-
-                    return null;
-                },
-                getData(response) {
-                    if (_.has(response, 'data')) {
-                        return this.getData(_.get(response, 'data'));
-                    }
-
+                if (_.isString(response)) {
                     return response;
-                },
-                getMeta(response) {
-                    if (_.has(response, 'meta')) {
-                        return this.getMeta(_.get(response, 'meta'));
-                    } else if (_.has(response, 'data')) {
-                        return this.getMeta(_.get(response, 'data'));
-                    }
+                }
 
-                    return response;
-                },
-            };
+                return null;
+            },
+            getData(response) {
+                if (_.has(response, 'data')) {
+                    return this.getData(_.get(response, 'data'));
+                }
+
+                return response;
+            },
+            getMeta(response) {
+                if (_.has(response, 'meta')) {
+                    return this.getMeta(_.get(response, 'meta'));
+                } else if (_.has(response, 'data')) {
+                    return this.getMeta(_.get(response, 'data'));
+                }
+
+                return response;
+            },
         };
-    },
+    }
 };
 
 export default VueGetData;
